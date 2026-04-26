87c85afa9c7c8021be6dd9c8b86f24eac161d599
Aurora <219875469+Aurora-Aeralis@users.noreply.github.com>
2026-04-26 06:45:01 +0300

    Harden message max-size compatibility reads

--- Tests/MessageCompressionBoundsTests.cs
+++ Tests/MessageCompressionBoundsTests.cs
@@ -21,21 +21,21 @@ public class MessageCompressionBoundsTests
     [Fact]
     public void DecompressPayload_Rejects_Data_Exceeding_Max_Output_Size()
     {
-        var original = new byte[Message.MaxSize + 1];
+        var original = new byte[Message.GetMaxSize() + 1];
         for (var i = 0; i < original.Length; i++) original[i] = (byte)(i % 7);
 
         using var source = new Message(1u, "compress", 0);
         source.WriteBytes(original);
         var compressed = source.CompressPayload();
 
-        var ex = Assert.Throws<InvalidDataException>(() => Message.DecompressPayload(compressed, Message.MaxSize));
+        var ex = Assert.Throws<InvalidDataException>(() => Message.DecompressPayload(compressed, Message.GetMaxSize()));
         Assert.Contains("exceeds max allowed size", ex.Message);
     }
 
     [Fact]
     public void DecompressPayload_Allows_Data_Within_Logical_Message_Cap()
     {
-        var original = new byte[Message.MaxSize + 1];
+        var original = new byte[Message.GetMaxSize() + 1];
         for (var i = 0; i < original.Length; i++) original[i] = (byte)(i % 11);
 
         using var source = new Message(1u, "compress", 0);
@@ -49,7 +49,7 @@ public class MessageCompressionBoundsTests
     [Fact]
     public void DecompressPayload_DefaultLimit_Allows_Data_Within_Logical_Message_Cap()
     {
-        var original = new byte[Message.MaxSize + 1];
+        var original = new byte[Message.GetMaxSize() + 1];
         for (var i = 0; i < original.Length; i++) original[i] = (byte)(i % 13);
 
         using var source = new Message(1u, "compress", 0);
